c95b9a17210c73d7fe8c7418ed7ae2ba0264a609
yyy <super.doter.yyy@gmail.com>
2013-12-08 16:04:35 +0800

    first commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>title</title>
+	<link href="dsfa.dsf" type="text/css" rel="stylesheet">
+	
+</head>
+<body>
+
+
+</body>
+</html>
